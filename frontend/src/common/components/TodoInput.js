058f6f32a36055a3a5e17e33cb2237c286d1e1f7
hyeminLee7 <hmin10580@gmail.com>
2021-09-28 17:16:18 +0900

    sep 28

--- frontend/src/common/components/TodoInput.js
+++ frontend/src/common/components/TodoInput.js
@@ -1,11 +1,12 @@
 import React, {useState} from "react";
 import { useDispatch } from 'react-redux'
 import { v4 as uuid4 } from 'uuid'
+import { addTodoAction } from "reducers/todo.reducer";
 
 export default function Todo(){
 
     const [todo, setTodo] = useState('')
-
+    const dispatch = useDispatch()
     const submitForm = e => {
         e.preventDefault()
         const newTodo = {
@@ -16,9 +17,7 @@ export default function Todo(){
         addTodo(newTodo)
         setTodo('')
     }
-
-    const addTodo = todo => dispatchEvent(addTodoAction)
-
+    const addTodo = todo => dispatch(addTodoAction(todo))
     const handleChange = e => {
         e.preventDefault()
         setTodo(e.target.value)
