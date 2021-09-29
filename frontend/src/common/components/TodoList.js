6607effc846e772f6979742713d1ee2a78f858b7
hyeminLee7 <hmin10580@gmail.com>
2021-09-29 12:41:16 +0900

    sep29

--- frontend/src/common/components/TodoList.js
+++ frontend/src/common/components/TodoList.js
@@ -1,7 +1,12 @@
 import React from "react";
+import { useSelector } from 'react-redux'
 
 export default function TodoList(){
+    const todos = useSelector(state => state.todoReducer.todos)
     return(
-        <h1 style={{textAlign:'center'}}>등록된 목록이 없습니다.</h1>
+        <div style={{textAlign:'center'}}>
+            {todos.length === 0 && (<h1 >등록된 할 일 목록이 없습니다.</h1>)}
+        {todos.length !== 0 && (<h1>{todos.length}개의 할 일 목록이 있습니다.</h1>)}
+        </div>
     )
 }
--- frontend/src/reducers/todo.reducer.js
+++ frontend/src/reducers/todo.reducer.js
@@ -2,6 +2,7 @@ const initialState = {todos:[], todo:{}}
 export const addTodoAction = todo => ({type : "ADD_TODO", payload: todo})
 const todoReducer = (state = initialState, aciton) => {
     switch(aciton.type){
+        case 'ADD_TODO' : return {...state, todos: [...state.todos, aciton.payload]}
         default : return state
     }
 }
