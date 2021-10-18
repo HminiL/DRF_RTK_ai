0e5cb1720d265f44d218b929797091e8dde04016
hyeminLee7 <hmin10580@gmail.com>
2021-10-18 18:16:05 +0900

    tensor_basic

--- backend/admin/settings.py
+++ backend/admin/settings.py
@@ -44,7 +44,7 @@ INSTALLED_APPS = [
     'admin.housing',
     'admin.crime',
     'admin.crawling',
-    'admin.calculator',
+    'admin.tensor',
 ]
 CORS_ORIGIN_WHITELIST = ['http://127.0.0.1:3000' ,'http://localhost:3000']
 CORS_ALLOW_CREDENTIALS = True
--- backend/admin/tensor/models.py
+++ backend/admin/tensor/models.py
@@ -2,27 +2,37 @@ import numpy as np
 import tensorflow as tf
 
 
+class Perceptron(object):
+    def __init__(self):
+        pass
+
+    def process(self):
+        pass
+    
+
 class Calculator(object):
 
     def __init__(self):
         print(f'Tenserflow Version: {tf.__version__}')
 
     def process(self):
-        self.plus(3,4)
-        self.mean(3,4)
+        print('*'*200)
+        self.plus(4,8)
+        print('*'*200)
+        self.mean()
 
     def plus(self, a, b):
         print(tf.constant(a) + tf.constant(b))
 
-    def mean(self, a, b):
-        x_array = np.array(18).reshape(3,2,3)
-        x2 = tf.reshape(x_array, shape=(-1, 6))
+    def mean(self):
+        x_array = np.arange(18).reshape(3,2,3)
+        x2 = tf.reshape(x_array, shape=(-1, 6)) # -1 은 그대로를 의미
         # 각 열의 합을 계산
         xsum = tf.reduce_sum(x2, axis=0)
         # 각 열의 평군을 계산
         xmean = tf.reduce_mean(x2, axis=0)
 
         print(f'입력 크기: {x_array.shape} \n')
-        print(f'크기가 변경된 입력 크기: {x2.numpy()}\n')
+        print(f'크기가 변경된 입력 크기: \n{x2.numpy()}\n')
         print(f'열의 합: {xsum.numpy()}\n')
         print(f'열의 평균: {xmean.numpy()}\n')
